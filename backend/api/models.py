04059efca70f4aeefb264e22f88d6cf34fbbf386
Zuzanna Kacprzak <zuziakacprzak2603@gmail.com>
2025-04-07 11:29:05 +0200

    feat: add custom user model and regenerate initial migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+    
