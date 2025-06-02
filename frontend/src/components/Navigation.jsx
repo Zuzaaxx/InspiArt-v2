d8dd335c8b7b84ae0831481b53ef0816e45f4766
Zuzanna Kacprzak <zuziakacprzak2603@gmail.com>
2025-06-02 16:03:57 +0200

    feat: add navigation

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Navigation.css';
+
+const Navigation = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('authToken');
+        navigate('/login');
+    };
+
+    return (
+        <nav className="navigation-container">
+            <div className="inspiart">
+                <span className="inspiart-text">InspiArt</span>
+                <span role="img" aria-label="feather">🪶</span>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li>
+                        <NavLink to="/start" className={({ isActive }) => isActive ? 'menu-button active' : 'menu-button'}>
+                            <i className="fa-solid fa-house"></i>
+                            <p className="menu-button-text">Start</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/favourites" className={({ isActive }) => isActive ? 'menu-button active' : 'menu-button'}>
+                            <i className="fa-solid fa-heart"></i>
+                            <p className="menu-button-text">Favourite ideas</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/gallery" className={({ isActive }) => isActive ? 'menu-button active' : 'menu-button'}>
+                            <i className="fa-solid fa-image"></i>
+                            <p className="menu-button-text">My gallery</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/profile" className={({ isActive }) => isActive ? 'menu-button active' : 'menu-button'}>
+                            <i className="fa-solid fa-user"></i>
+                            <p className="menu-button-text">My profile</p>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            <button className="log-out" onClick={handleLogout}>Log out</button>
+        </nav>
+    );
+};
+
+export default Navigation;
