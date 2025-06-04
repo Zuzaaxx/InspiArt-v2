5085cd0e02ea8a72b9829f9bae229c484558fba9
Zuzanna Kacprzak <zuziakacprzak2603@gmail.com>
2025-06-04 12:14:31 +0200

    fix: logo-bird on signup page

--- frontend/src/pages/JoinPage.jsx
+++ frontend/src/pages/JoinPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import RegisterForm from '../components/RegisterForm';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container">
       <div className="login-container">
@@ -15,7 +18,16 @@ const SignUpPage = () => {
 
       <div className="base-container">
         <div className="top-panel">
-          <img className="decoration-top" alt="" src="/src/assets/Vector 1.svg" />
+          <div className="sign-up-section">
+            <div className="still-dont-have">Already have an account?</div>
+            <button className="sign-up" onClick={() => navigate('/login')}>
+              Log in
+            </button>
+          </div>
+        </div>
+
+        <div className="logo-picture">
+          <img src="/src/assets/logo-bird.png" alt="Logo Bird" />
         </div>
 
         <img className="decoration-bottom" alt="" src="/src/assets/Vector 2.svg" />
